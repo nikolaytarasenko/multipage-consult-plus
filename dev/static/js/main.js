eca9c0a1594bd3d98d7660fffc1378da047712b7
Nikolay Tarasenko <kolyatarasenko@ukr.net>
2019-09-19 19:55:47 +0300

    fix pug compile, banner section was added

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,3 +1,13 @@
 $(document).ready(function () {
     svg4everybody({});
+
+    $('.slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+    })
 });
