ec67d9a8796686f3815f0a4061bfa9b060c695ad
Nikolay Tarasenko <kolyatarasenko@ukr.net>
2019-09-20 22:39:32 +0300

    testimonials section was added

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -8,6 +8,31 @@ $(document).ready(function () {
         slidesToShow: 1,
         adaptiveHeight: true,
         autoplay: true,
-        autoplaySpeed: 5000,
-    })
+        autoplaySpeed: 5000
+    });
+
+    $('.testimonials__list').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        autoplay: true,
+        autoplaySpeed: 3000
+    }).on('afterChange', function(event) {
+        const sliderDots = document.querySelectorAll('.slick-dots li');
+        const clients = document.querySelectorAll('.client');
+        let activeDotIndex;
+
+        Array.from(sliderDots).forEach((item, index) => {
+            if (item.classList.contains('slick-active')) {
+                activeDotIndex = index;
+            }
+        });
+
+        for (let i = 0; i < clients.length; i++) {
+            clients[i].style.opacity = '0.5';
+        }
+
+        clients[activeDotIndex].style.opacity = '1';
+    });
 });
