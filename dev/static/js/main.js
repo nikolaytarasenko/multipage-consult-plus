1278e406a9460a611abf461af6e440b506a70cd9
Nikolay Tarasenko <kolyatarasenko@ukr.net>
2019-09-22 23:03:31 +0300

    careers page: modal window for hiring positions was added

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -35,4 +35,53 @@ $(document).ready(function () {
 
         clients[activeDotIndex].style.opacity = '1';
     });
+
+    // modal window om career page
+    const applyPosition = () => {
+        const applyButtons = document.querySelectorAll('.positions__btn');
+        const modal = document.querySelector('.modal');
+        const closeButton = document.querySelector('.modal__close');
+        const modalTitle = document.querySelector('.modal__title');
+        const form = document.querySelector('.modal__form');
+        const submitButton = document.querySelector('.modal__btn');
+        const overlay = document.querySelector('.overlay');
+
+        for (let i = 0; i < applyButtons.length; i++) {
+            applyButtons[i].addEventListener('click', e => {
+                e.preventDefault();
+
+                const message = modal.querySelector('.modal__message');
+
+                if (message) message.parentElement.removeChild(message);
+
+                overlay.classList.add('js-overlay-show');
+                modal.classList.add('js-show');
+
+                modalTitle.textContent = e.target.parentElement.querySelector('.positions__name').textContent;
+            });
+        }
+
+        closeButton.addEventListener('click', e => {
+            modal.classList.remove('js-show');
+            overlay.classList.remove('js-overlay-show');
+            form.classList.remove('js-hide');
+        });
+
+        submitButton.addEventListener('click', e => {
+            e.preventDefault();
+
+            const text = document.createElement('div');
+            text.className = 'modal__message';
+            text.textContent = 'Thank you! Your data was sent.';
+            form.classList.add('js-hide');
+            modal.appendChild(text);
+        });
+    };
+
+    // initialize the main functionality
+    const init = () => {
+        applyPosition();
+    };
+
+    init();
 });
